31034975999a70ce4a834ad92dad186a77fb053d
Rahul Singh <docoderahul@gmail.com>
2025-05-01 13:29:06 +0530

    Added some usercontroller and user routes

--- src/app.js
+++ src/app.js
@@ -9,7 +9,14 @@ app.use(
     credentials: true,
   })
 );
-app.use(express.urlencoded({ extended, limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.static("public"));
+app.use(cookieParser());
+
+//routes
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
+
 export default app;
