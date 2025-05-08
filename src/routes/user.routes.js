cb371f358fa46035b0f86802d148598dcc2f90e7
Rahul Singh <docoderahul@gmail.com>
2025-05-08 17:29:24 +0530

    feat: add user profile and account management routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
+  currentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginOut,
   loginUser,
   refreshAcessToken,
   registerUser,
+  updateAccountDetails,
+  updateAvatar,
+  updateCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.midleware.js";
@@ -23,4 +30,17 @@ router.route("/login").post(loginUser);
 // verifyJWT is middleware as in last we mention next()
 router.route("/logout").post(verifyJWT, loginOut);
 router.route("/refresh-token").post(refreshAcessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, currentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/change-avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateAvatar);
+
+router
+  .route("/update-cover")
+  .patch(verifyJWT, upload.single("/coverImage"), updateCoverImage);
+
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 export default router;
